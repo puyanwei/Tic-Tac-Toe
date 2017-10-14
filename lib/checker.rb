7d2924696dc12a0c48706b534bda9a88056eb743
pyan83 <puyanwei@gmail.com>
2017-10-14 21:07:27 +0100

    created full coverage tests for all winning combinations

--- lib/checker.rb
+++ lib/checker.rb
@@ -9,7 +9,8 @@ class Checker
   end
 
   def check_winner
-    top_row? || middle_row? || bottom_row?
+    top_row? || middle_row? || bottom_row? || left_col? || middle_col? || right_col? || diag_down? || diag_up?
+
   end
 
   def top_row?
@@ -23,9 +24,24 @@ class Checker
   def bottom_row?
     @state[6..8] == @ohs || @state[6..8] == @exes
   end
-end
 
-# what are wins?
-# across 123, 456, 789,
-# downs 147, 258, 369
-# diagonals 159, 357
+  def left_col?
+    @ohs == [@state[0], @state[3], @state[6]] || @exes == [@state[0], @state[3], @state[6]]
+  end
+
+  def middle_col?
+    @ohs == [@state[1], @state[4], @state[7]] || @exes == [@state[1], @state[4], @state[7]]
+  end
+
+  def right_col?
+    @ohs == [@state[2], @state[5], @state[8]] || @exes == [@state[2], @state[5], @state[8]]
+  end
+
+  def diag_down?
+    @ohs == [@state[0], @state[4], @state[8]] || @exes == [@state[0], @state[4], @state[8]]
+  end
+
+  def diag_up?
+    @ohs == [@state[2], @state[4], @state[6]] || @exes == [@state[2], @state[4], @state[6]]
+  end
+end
--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Checker do
-  subject(:checker) { described_class.new(1) }
-  # let (:state) { double :state }
+  subject(:checker) { described_class.new(state) }
+  let(:state) { double :state }
 
   describe "#check_winner" do
     it 'checks the board to see if top row wins with O' do
@@ -18,6 +18,31 @@ RSpec.describe Checker do
       expect(checker.check_winner).to be(true)
     end
 
+    it 'checks the board to see if left column wins with O' do
+      checker.state = ["O", 0, 0, "O", 0, 0, "O", 0, 0]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if middle column wins with O' do
+      checker.state = [0, "O", 0, 0, "O", 0, 0, "O", 0]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if right column wins with O' do
+      checker.state = [0, 0, "O", 0, 0, "O", 0, 0, "O"]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if diagonal down left to right wins with O' do
+      checker.state = ["O", 0, 0, 0, "O", 0, 0, 0, "O"]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if diagonal up left to right wins with O' do
+      checker.state = [0, 0, "O", 0, "O", 0, "O", 0, 0]
+      expect(checker.check_winner).to be(true)
+    end
+
     it 'checks the board to see if top row wins with X' do
       checker.state = ["X", "X", "X", 0, 0, 0, 0, 0, 0]
       expect(checker.check_winner).to be(true)
@@ -30,6 +55,36 @@ RSpec.describe Checker do
 
     it 'checks the board to see if bottom row wins with X' do
       checker.state = [0, 0, 0, 0, 0, 0, "X", "X", "X"]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if left column wins with X' do
+      checker.state = ["X", 0, 0, "X", 0, 0, "X", 0, 0]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if left column wins with X' do
+      checker.state = ["X", 0, 0, "X", 0, 0, "X", 0, 0]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if middle column wins with X' do
+      checker.state = [0, "X", 0, 0, "X", 0, 0, "X", 0]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if right column wins with X' do
+      checker.state = [0, 0, "X", 0, 0, "X", 0, 0, "X"]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if diagonal down left to right wins with X' do
+      checker.state = ["X", 0, 0, 0, "X", 0, 0, 0, "X"]
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if diagonal up left to right wins with X' do
+      checker.state = [0, 0, "X", 0, "X", 0, "X", 0, 0]
       expect(checker.check_winner).to be(true)
     end
   end
