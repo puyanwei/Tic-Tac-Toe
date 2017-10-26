3b4ca172809eb0efb10c0db7188cec8317cd3124
pyan83 <puyanwei@gmail.com>
2017-10-26 08:49:59 +0100

    Checker class tested all winning combinations

--- lib/checker.rb
+++ lib/checker.rb
@@ -7,39 +7,38 @@ class Checker
   end
 
   def check_winner
-    top_row? || middle_row?
-    # || bottom_row? || left_col? || middle_col? || right_col? || diag_down? || diag_up?
+    top_row? || middle_row? || bottom_row? || left_col? || middle_col? || right_col? || diag_down? || diag_up?
   end
 
   def top_row?
-      @state[1] == "O" && @state[4] == "O" && @state[7] == "O" || @state[1] == "X" && @state[4] == "X" && @state[7] == "X"
+    @state[1] == "O" && @state[4] == "O" && @state[7] == "O" || @state[1] == "X" && @state[4] == "X" && @state[7] == "X"
   end
 
   def middle_row?
-      @state[11] == "O" && @state[14] == "O" && @state[17] == "O" || @state[11] == "X" && @state[14] == "X" && @state[17] == "X"
+    @state[11] == "O" && @state[14] == "O" && @state[17] == "O" || @state[11] == "X" && @state[14] == "X" && @state[17] == "X"
   end
-  #
-  # def bottom_row?
-  #   @state[6..8] == @ohs || @state[6..8] == @exes
-  # end
-  #
-  # def left_col?
-  #   @ohs == [@state[0], @state[3], @state[6]] || @exes == [@state[0], @state[3], @state[6]]
-  # end
-  #
-  # def middle_col?
-  #   @ohs == [@state[1], @state[4], @state[7]] || @exes == [@state[1], @state[4], @state[7]]
-  # end
-  #
-  # def right_col?
-  #   @ohs == [@state[2], @state[5], @state[8]] || @exes == [@state[2], @state[5], @state[8]]
-  # end
-  #
-  # def diag_down?
-  #   @ohs == [@state[0], @state[4], @state[8]] || @exes == [@state[0], @state[4], @state[8]]
-  # end
-  #
-  # def diag_up?
-  #   @ohs == [@state[2], @state[4], @state[6]] || @exes == [@state[2], @state[4], @state[6]]
-  # end
+
+  def bottom_row?
+    @state[21] == "O" && @state[24] == "O" && @state[27] == "O" || @state[21] == "X" && @state[24] == "X" && @state[27] == "X"
+  end
+
+  def left_col?
+    @state[1] == "O" && @state[11] == "O" && @state[21] == "O" || @state[1] == "X" && @state[11] == "X" && @state[21] == "X"
+  end
+
+  def middle_col?
+    @state[4] == "O" && @state[14] == "O" && @state[24] == "O" || @state[4] == "X" && @state[14] == "X" && @state[24] == "X"
+  end
+
+  def right_col?
+    @state[7] == "O" && @state[17] == "O" && @state[27] == "O" || @state[7] == "X" && @state[17] == "X" && @state[27] == "X"
+  end
+
+  def diag_down?
+    @state[1] == "O" && @state[14] == "O" && @state[27] == "O" || @state[1] == "X" && @state[14] == "X" && @state[27] == "X"
+  end
+
+  def diag_up?
+    @state[7] == "O" && @state[14] == "O" && @state[21] == "O" || @state[7] == "X" && @state[14] == "X" && @state[21] == "X"
+  end
 end
--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -13,36 +13,36 @@ RSpec.describe Checker do
       expect(checker.check_winner).to be(true)
     end
 
-#     it 'checks the board to see if bottom row wins with O' do
-#       checker.state = [0, 0, 0, 0, 0, 0, "O", "O", "O"]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if left column wins with O' do
-#       checker.state = ["O", 0, 0, "O", 0, 0, "O", 0, 0]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if middle column wins with O' do
-#       checker.state = [0, "O", 0, 0, "O", 0, 0, "O", 0]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if right column wins with O' do
-#       checker.state = [0, 0, "O", 0, 0, "O", 0, 0, "O"]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if diagonal down left to right wins with O' do
-#       checker.state = ["O", 0, 0, 0, "O", 0, 0, 0, "O"]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if diagonal up left to right wins with O' do
-#       checker.state = [0, 0, "O", 0, "O", 0, "O", 0, 0]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
+    it 'checks the board to see if bottom row wins with O' do
+      checker.state = "[ ][ ][ ]\n[ ][ ][ ]\n[O][O][O]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if left column wins with O' do
+      checker.state = "[O][ ][ ]\n[O][ ][ ]\n[O][ ][ ]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if middle column wins with O' do
+      checker.state = "[ ][O][ ]\n[ ][O][ ]\n[ ][O][ ]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if right column wins with O' do
+      checker.state = "[ ][ ][O]\n[ ][ ][O]\n[ ][ ][O]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if diagonal down left to right wins with O' do
+      checker.state = "[O][ ][ ]\n[ ][O][ ]\n[ ][ ][O]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if diagonal up left to right wins with O' do
+      checker.state = "[ ][ ][O]\n[ ][O][ ]\n[O][ ][ ]"
+      expect(checker.check_winner).to be(true)
+    end
+
     it 'checks the board to see if top row wins with X' do
       checker.state = "[X][X][X]\n[ ][ ][ ]\n[ ][ ][ ]"
       expect(checker.check_winner).to be(true)
@@ -52,40 +52,40 @@ RSpec.describe Checker do
       checker.state = "[ ][ ][ ]\n[X][X][X]\n[ ][ ][ ]"
       expect(checker.check_winner).to be(true)
     end
-#
-#     it 'checks the board to see if bottom row wins with X' do
-#       checker.state = [0, 0, 0, 0, 0, 0, "X", "X", "X"]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if left column wins with X' do
-#       checker.state = ["X", 0, 0, "X", 0, 0, "X", 0, 0]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if left column wins with X' do
-#       checker.state = ["X", 0, 0, "X", 0, 0, "X", 0, 0]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if middle column wins with X' do
-#       checker.state = [0, "X", 0, 0, "X", 0, 0, "X", 0]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if right column wins with X' do
-#       checker.state = [0, 0, "X", 0, 0, "X", 0, 0, "X"]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if diagonal down left to right wins with X' do
-#       checker.state = ["X", 0, 0, 0, "X", 0, 0, 0, "X"]
-#       expect(checker.check_winner).to be(true)
-#     end
-#
-#     it 'checks the board to see if diagonal up left to right wins with X' do
-#       checker.state = [0, 0, "X", 0, "X", 0, "X", 0, 0]
-#       expect(checker.check_winner).to be(true)
-#     end
+
+    it 'checks the board to see if bottom row wins with X' do
+      checker.state = "[ ][ ][ ]\n[ ][ ][ ]\n[X][X][X]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if left column wins with X' do
+      checker.state = "[X][ ][ ]\n[X][ ][ ]\n[X][ ][ ]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if left column wins with X' do
+      checker.state = "[ ][X][ ]\n[ ][X][ ]\n[ ][X][ ]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if middle column wins with X' do
+      checker.state = "[ ][ ][X]\n[ ][ ][X]\n[ ][ ][X]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if right column wins with X' do
+      checker.state = "[X][ ][ ]\n[ ][X][ ]\n[ ][ ][X]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if diagonal down left to right wins with X' do
+      checker.state = "[X][ ][ ]\n[ ][X][ ]\n[ ][ ][X]"
+      expect(checker.check_winner).to be(true)
+    end
+
+    it 'checks the board to see if diagonal up left to right wins with X' do
+      checker.state = "[ ][ ][X]\n[ ][X][ ]\n[X][ ][ ]"
+      expect(checker.check_winner).to be(true)
+    end
   end
 end
